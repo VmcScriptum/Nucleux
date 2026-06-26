0fcd423f941057cb59fd7912506f5c70cfbaa68f
VMCPLAYS <149696131+VmcPlays@users.noreply.github.com>
2026-06-25 21:27:21 -0300

    mudança na logo

--- install.sh
+++ install.sh
@@ -1,17 +1,31 @@
 #!/usr/bin/env bash
 
-# 1. PARADA DE EMERGÊNCIA: Se qualquer comando der erro, o script morre na hora. 
-# (Sem isso, se a internet cair no meio do download do driver, ele continua rodando e destrói o sistema).
+
 set -e
 
-# 2. Paleta de cores para o terminal (dar aquele visual de "Hacker de cinema")
+
 VERDE='\033[0;32m'
 VERMELHO='\033[0;31m'
 AZUL='\033[0;34m'
 AMARELO='\033[1;33m'
 RESET='\033[0m'
 
-# Funções de padronização de mensagens
+print_logo() {
+    clear
+    echo -e "${AZUL}"
+    cat << "EOF"
+         .--.
+        |o_o |       ███╗   ██╗██╗   ██╗██████╗ ██╗     ███████╗██╗   ██╗██╗  ██╗
+        |:_/ |       ████╗  ██║██║   ██║██╔════╝██║     ██╔════╝██║   ██║╚██╗██╔╝
+       //   \ \      ██╔██╗ ██║██║   ██║██║     ██║     █████╗  ██║   ██║ ╚███╔╝ 
+      (|     | )     ██║╚██╗██║██║   ██║██║     ██║     ██╔══╝  ██║   ██║ ██╔██╗ 
+     /'\_   _/`\     ██║ ╚████║╚██████╔╝╚██████╗███████╗███████╗╚██████╔╝██╔╝ ██╗
+     \___)=(___/     ╚═╝  ╚═══╝ ╚═════╝  ╚═════╝╚══════╝╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+                                  [ Automated Dev OS ]
+EOF
+    echo -e "${RESET}"
+}
+
 msg_info() {
     echo -e "${AZUL}[INFO]${RESET} $1"
 }
@@ -26,25 +40,20 @@ msg_erro() {
 }
 
 clear
-echo -e "${VERDE}"
-echo "================================================="
-echo "        INSTALADOR DO MEU ARCH v0.1              "
-echo "================================================="
-echo -e "${RESET}"
+print_logo
 
-# 3. VERIFICAÇÃO 1: O usuário cometeu o erro de rodar como 'sudo ./install.sh'?
 if [[ $EUID -eq 0 ]]; then
    msg_erro "Este script NÃO pode ser rodado como root! Execute apenas: ./install.sh"
 fi
 
-# 4. VERIFICAÇÃO 2: Tem internet?
+
 msg_info "Testando conexão com os servidores do Arch..."
 if ! ping -c 1 archlinux.org &> /dev/null; then
     msg_erro "Sem conexão com a internet. Verifique sua rede na VM e tente de novo."
 fi
 msg_ok "Conexão com a internet confirmada."
 
-# 5. VERIFICAÇÃO 3: O pacman está travado por outra atualização em segundo plano?
+
 if [ -f /var/lib/pacman/db.lck ]; then
     msg_info "Trava do Pacman encontrada. Removendo..."
     sudo rm /var/lib/pacman/db.lck
@@ -52,3 +61,24 @@ if [ -f /var/lib/pacman/db.lck ]; then
 fi
 
 msg_ok "Ambiente verificado com sucesso. Pronto para iniciar a instalação."
+
+msg_info "Verificando se o 'yay' já está instalado..."
+
+if ! command -v yay &> /dev/null; then
+    msg_info "O 'yay' não foi encontrado. Iniciando compilação automática..."
+    
+    cd /tmp
+    
+    rm -rf yay-bin
+
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    
+    makepkg -si --noconfirm
+    
+    cd - &> /dev/null
+    
+    msg_ok "Gerenciador 'yay' instalado com sucesso!"
+else
+    msg_ok "O 'yay' já está instalado no sistema. Pulando etapa."
+fi
